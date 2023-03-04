df0994fcac7b98f423541ecae60b50361a248641
Leo Martinez III <123003509+LeoMartinezTAMUK@users.noreply.github.com>
2023-03-04 16:18:08 -0600

    Add files via upload

---
+++ FinalProjectPL/src/Test.java
@@ -0,0 +1,119 @@
+
+// *** case 4 of Test.java needs to be revised ***
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        int choice = 0, enterPin;
+        long enterAccountNo;
+
+        try (Scanner scanner = new Scanner(System.in)) {
+			atm user1 = new atm();
+			user1.setData(1234567, "Tim", 1111, 45000.90, "9087654321");
+
+			do {
+			    clearScreen();
+
+			    System.out.println("\n****Welcome to the ATM****");
+			    System.out.print("\nEnter Your Account No: ");
+			    enterAccountNo = scanner.nextLong();
+			    System.out.print("Enter PIN: ");
+			    enterPin = scanner.nextInt();
+
+			    if (enterAccountNo == user1.getAccountNo() && enterPin == user1.getPin()) {
+			        do {
+			            int amount = 0;
+			            //String oldMobileNo, newMobileNo;
+			            //char menu;
+
+			            clearScreen();
+
+			            System.out.println("\n**** Welcome to the ATM ****");
+			            System.out.println("\nSelect Options");
+			            System.out.println("1. Check Balance");
+			            System.out.println("2. Cash Withdrawal");
+			            System.out.println("3. Show User Details");
+			            System.out.println("4. Update Mobile No.");
+			            System.out.println("5. Exit");
+			            choice = scanner.nextInt();
+
+			            switch (choice) {
+			                case 1:
+			                    clearScreen();
+			                    System.out.println("**** Check Balance ****");
+			                    System.out.println("\nYour Bank balance is: $" + user1.getBalance());
+			                    System.out.print("\nPress ENTER for Main Menu");
+			                    scanner.nextLine();
+			                    scanner.nextLine();
+			                    break;
+
+			                case 2:
+			                    clearScreen();
+			                    System.out.println("**** Cash Withdrawal ****");
+			                    System.out.print("\nEnter the amount: ");
+			                    amount = scanner.nextInt();
+			                    user1.cashWithdraw(amount);
+			                    System.out.print("\nPress ENTER for Main Menu");
+			                    scanner.nextLine();
+			                    scanner.nextLine();
+			                    break;
+
+			                case 3:
+			                    clearScreen();
+			                    System.out.println("**** User Details ****");
+			                    System.out.println("\n-> Account No: " + user1.getAccountNo());
+			                    System.out.println("\n-> Name: " + user1.getName());
+			                    System.out.println("\n-> Balance: $" + user1.getBalance());
+			                    System.out.println("\n-> Mobile No.: " + user1.getMobileNo());
+			                    System.out.print("\nPress ENTER for Main Menu");
+			                    scanner.nextLine();
+			                    scanner.nextLine();
+			                    break;
+			                    
+			                case 4:		// *** NEEDS FIXING *** // 
+			                    clearScreen();
+			                    System.out.println("**** Update Mobile Number ****\n");
+
+			                    // Take old mobile number
+			                    System.out.print("Enter Old Mobile No. ");
+			                    String prevMobileNo = scanner.nextLine();
+			                
+			                    // Take new mobile number
+			                    System.out.print("\nEnter New Mobile No. ");
+			                    String newMobileNo = scanner.nextLine();
+
+			                    // Set the new mobile number
+			                    user1.setMobile(prevMobileNo, newMobileNo);
+
+			                    // Pause until ENTER is pushed
+			                    System.out.print("\nPress ENTER for Main Menu");
+			                    scanner.nextLine();
+			                    break;
+
+			                case 5:
+			                    System.exit(0);
+
+			                default:
+			                    System.out.println("\nInvalid choice. Please select a valid option.");
+			                    System.out.print("\nPress ENTER to try again");
+			                    scanner.nextLine();
+			                    scanner.nextLine();
+			            }
+			        } while (true);
+			    } else {
+			        System.out.println("\nInvalid account number or PIN. Please try again.");
+			        System.out.print("\nPress ENTER to try again");
+			        scanner.nextLine();
+			        scanner.nextLine();
+			    }
+			} while (true);
+		}
+    }
+
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+}
+   
---
+++ FinalProjectPL/src/atm.java
@@ -0,0 +1,74 @@
+/* Program was written in Java and tested in Eclipse IDE Version: 2022-12 (4.26.0)
+Java version "19.0.2" 2023-01-17
+Java(TM) SE Runtime Environment (build 19.0.2+7-44)
+Java HotSpot(TM) 64-Bit Server VM (build 19.0.2+7-44, mixed mode, sharing)
+	
+Final Project Prototype/Template Programming Languages CSEN 4366
+Group 1: Leo Martinez III, Jalen Williams, Amelia Trevino	*/
+
+// *** case 4 of Test.java needs to be revised ***
+
+/* ATM
+-> Check Balance
+-> Cash withdraw
+-> User Details
+-> Update Mobile No.
+*/
+
+class atm {
+private long accountNo;
+private String name;
+private int pin;
+private double balance;
+private String mobileNo;
+
+//
+public void setData(long accountNo, String name, int pin, double balance, String mobileNo) {
+    this.accountNo = accountNo;
+    this.name = name;
+    this.pin = pin;
+    this.balance = balance;
+    this.mobileNo = mobileNo;
+}
+
+public long getAccountNo() {
+    return accountNo;
+}
+
+public String getName() {
+    return name;
+}
+
+public int getPin() {
+    return pin;
+}
+
+public double getBalance() {
+    return balance;
+}
+
+public String getMobileNo() {
+    return mobileNo;
+}
+
+public void setMobile(String prevMobileNo, String newMobileNo) {
+    if (prevMobileNo.equals(mobileNo)) {
+        mobileNo = newMobileNo;
+        System.out.println("\nSuccessfully Updated Mobile No.");
+    } else {
+        System.out.println("\nIncorrect!!! Old Mobile No.");
+    }
+}
+
+public void cashWithdraw(int amount) {
+    if (amount > 0 && amount < balance) {
+        balance -= amount;
+        System.out.println("\nPlease Collect Your Cash");
+        System.out.println("Available Balance: $" + balance);
+    } else {
+        System.out.println("\nInvalid Input or Insufficient Balance");
+    }
+}
+
+//
+}
